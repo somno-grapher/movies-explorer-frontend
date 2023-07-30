57345497422b9b14d1bdc06de7b37061ba710d15
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-07-30 11:33:45 +0300

    refactor: useLocation for Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Routes, Route, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import logoPath from '../../images/logo/logo.svg';
 
@@ -23,67 +23,56 @@ function Header({
         alt="Логотип"
       />
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="header__unauthorized">
-              <Link
-                className="header__item header__item_type_regular"
-                to="/signup"
-              >
-                Регистрация
-              </Link>
-              <Link
-                className="header__item header__item_type_signin"
-                to="/signin"
-              >
-                Войти
-              </Link>
-            </div>}
-        />
-      </Routes>
+      {location.pathname === "/" && (
+        <div className="header__unauthorized">
+          <Link
+            className="header__item header__item_type_regular"
+            to="/signup"
+          >
+            Регистрация
+          </Link>
+          <Link
+            className="header__item header__item_type_signin"
+            to="/signin"
+          >
+            Войти
+          </Link>
+        </div>)}
 
-      <Routes>
-        <Route
-          path="/movies"
-          element={
-            <div className="header__authorized">
-              <nav className="header__navbar">
-                <Link
-                  className="header__item header__item_type_movies"
-                  to="/movies"
-                >
-                  Фильмы
-                </Link>
-                <Link
-                  className="header__item header__item_type_regular"
-                  to="/saved-movies"
-                >
-                  Сохраненные фильмы
-                </Link>
-              </nav>
-              <Link
-                className="header__account"
-                to="/profile"
-              // onClick={onSignOut}
-              >
-                <p className="header__account-title">
-                  Аккаунт
-                </p>
-                <img className="header__account-icon" src="" alt="Иконка аккаунта" />
-              </Link>
-              {/* <Link
+      {location.pathname === "/movies" && (
+        <div className="header__authorized">
+          <nav className="header__navbar">
+            <Link
+              className="header__item header__item_type_movies"
+              to="/movies"
+            >
+              Фильмы
+            </Link>
+            <Link
+              className="header__item header__item_type_regular"
+              to="/saved-movies"
+            >
+              Сохраненные фильмы
+            </Link>
+          </nav>
+          <Link
+            className="header__account"
+            to="/profile"
+          // onClick={onSignOut}
+          >
+            <p className="header__account-title">
+              Аккаунт
+            </p>
+            <img className="header__account-icon" src="" alt="Иконка аккаунта" />
+          </Link>
+          {/* <Link
                   className="header__sign-out-link"
                   to="/signin"
                   onClick={onSignOut}
                 >
                   Выйти
                 </Link> */}
-            </div>
-          }
-        />
-      </Routes>
+        </div>)}
 
     </header >
   );
