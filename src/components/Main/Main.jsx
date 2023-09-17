c2ddfb295cefc4fdc6cdb38b92a9d68791f6e686
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-09-17 20:13:37 +0300

    fix: Main layout 520px

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -33,42 +33,6 @@ function Main({
       <Techs />
       <AboutMe />
       <Portfolio />
-      {/* <section className="profile content__profile">
-        <button className="profile__avatar-container"
-          onClick={handleEditAvatarClick}>
-          <img src={currentUser.avatar}
-            alt="Аватар"
-            className="avatar" />
-        </button>
-        <div className="profile__info">
-          <h1 className="profile__name">{currentUser.name}</h1>
-          <button type="button"
-            className="edit-button profile__edit-button edit-button_type_profile"
-            onClick={handleEditProfileClick}>
-          </button>
-          <p className="profile__about">{currentUser.about}</p>
-        </div>
-        <button type="button"
-          className="add-button profile__add-button add-button_type_card"
-          onClick={handleAddPlaceClick}>
-        </button>
-      </section>
-      <section className="photo-grid">
-        <ul className="photo-grid__list">
-          {cards.slice(0).reverse().map((card) => {
-            // {cards.map((card) => {
-            return (
-              <Card
-                key={card._id}
-                card={card}
-                onCardClick={onCardClick}
-                onCardDelete={onCardDelete}
-                onCardLike={onCardLike}
-              />
-            )
-          })}
-        </ul>
-      </section> */}
     </main>
   );
 
