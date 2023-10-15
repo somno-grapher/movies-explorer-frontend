3354f89fb1833d381908677e3f16ea53a2956fef
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-10-15 19:30:02 +0300

    hotfix: set email pattern

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -1,6 +1,5 @@
 const MAIN_API_BASE_PATH = 'https://api.d-movies-explorer.nomoredomains.work';
 const MOVIES_API_BASE_PATH = 'https://api.nomoreparties.co/beatfilm-movies';
-// ! enhance pattern
 const EMAIL_PATTERN = '\\S+@\\S+.\\S+';
 
 export {
