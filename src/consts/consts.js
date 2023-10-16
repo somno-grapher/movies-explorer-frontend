5f576cdc07062b9a3f411320148ccedeb5eb73af
Nikolay Mayornikov <nikolayvm@yandex.ru>
2023-10-16 17:41:47 +0300

    hotfix: fix email pattern

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -1,6 +1,6 @@
 const MAIN_API_BASE_PATH = 'https://api.d-movies-explorer.nomoredomains.work';
 const MOVIES_API_BASE_PATH = 'https://api.nomoreparties.co/beatfilm-movies';
-const EMAIL_PATTERN = '\\S+@\\S+.\\S+';
+const EMAIL_PATTERN = '^[\\w\\-\\.]+@([\\w\\-]+\\.)+[\\w\\-]{2,4}$';
 
 export {
   MAIN_API_BASE_PATH,
